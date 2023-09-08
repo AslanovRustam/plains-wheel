db5ae2c7db507b5dd6b3ca82e5ff9047b1d11da0
Aslanov Rustam <aslanovrg@gmail.com>
2023-09-08 16:48:39 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Container from "./components/Container/Container";
+import Main from "./components/Main/Main";
+import Wheel from "./components/Wheel/Wheel";
+
+function App() {
+  return (
+    <Container>
+      <Main />
+      <Wheel />
+    </Container>
+  );
+}
+
+export default App;
